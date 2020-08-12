a95418a83349f8eda57ddba6c6b2d1092f73e32d
parkkuyhee <52224109+parkkuyhee@users.noreply.github.com>
2020-08-13 07:45:37 +0900

    savinguser

--- gretting.js
+++ gretting.js
@@ -2,17 +2,34 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
-const USER_LS="currentUser"
+const USER_LS="currentUser",
+    SHOWING_CN = "showing";
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGretting(currentValue);
+};
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+};
+
 
 function paintGretting(text){
-
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
 }
 
 function loadName(){
-    const currentUser = loacalStorage.getItme(USER_LS);
-    if (currentUser==null){
-        
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser===null){
+        askForName();
 
+    }else{
+        paintGretting(currentUse);
     }
 }
 
