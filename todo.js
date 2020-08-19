69e45f9e99ba19ea2d3eadbc8c01aeb0fc623888
parkkuyhee <52224109+parkkuyhee@users.noreply.github.com>
2020-08-19 22:25:24 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,17 +2,32 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
-const TODO_LS='toDos';
+const TODO_LS="toDos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODO_LS,JSON.stringify(toDos));
+}
 
 function paintToDo(text){
    const li = document.createElement("li");
    const delBtn = document.createElement("button");
-   delBtn.innerText ="X";
    const span = document.createElement("span");
+   const newId = toDos.length+1;
+   delBtn.innerText ="X";
    span.innerText =text;
    li.appendChild(delBtn);
    li.appendChild(span);
+   li.id = newId;
    toDoList.appendChild(li);
+   const toDoObj ={
+        text : text,
+        id : newId
+   };
+
+   toDos.push(toDoObj);
+   saveToDos();
 }
 
 function handleSubmit(event){
@@ -23,9 +38,12 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODO_LS);
-    if(toDos!==null){
-
+    const loadedToDos = localStorage.getItem(TODO_LS);
+    if(loadedToDos!==null){
+       const parsedToDos = JSON.parse(loadedToDos);
+       parsedToDos.forEach(function(toDo){
+        paintToDo(toDo.text);
+       });
     }
 }
 
